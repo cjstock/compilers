69eaf241806bc71638368f1299a8485a97ec2a32
Corey <coreystock@gmail.com>
2019-09-26 15:47:16 -0700

    test with vscode

--- lexer.py
+++ lexer.py
@@ -15,11 +15,11 @@ class token:
 class lexer:
     def __init__(self):
         """Initializes the datastructures containing separators, keywords, and operators"""
-        self.separators = ['%%','(',')',',','{','}',';','=','[*','*]']
+        self.separators = ['\%\%','(',')',',','{','}',';','=','[*','*]']
         self.keywords = ['function','int','boolean','real','if','fi','otherwise','return','put','get','while','true','false']
         self.operators = ['==','/=','>','<','=>','<=','+','-','*','/']
 
     def parse(path):
         """Begins parsing the Rat19F file"""
         with open(path) as file:
-            
+            pass
