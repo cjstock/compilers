1acd1b84ecd14793b6a7db3bbc501a4d436ad317
Corey <coreystock@gmail.com>
2019-09-26 15:26:09 -0700

    added some comments

--- lexer.py
+++ lexer.py
@@ -10,11 +10,13 @@ import sys
 
 class token:
     def __init__(self, t_type, t_value):
+        """Initializes an instance of the token class"""
         self.t_type = t_type
         self.t_value = t_value
 
 class lexer:
     def __init__(self):
+        """Initializes the datastructures containing separators, keywords, and operators"""
         self.separators = ['%%','(',')',',','{','}',';','=','[*','*]']
         self.keywords = ['function','int','boolean','real','if','fi','otherwise','return','put','get','while','true','false']
         self.operators = ['==','/=','>','<','=>','<=','+','-','*','/']
