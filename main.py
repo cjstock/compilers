98fd24c9fc249f9ffe44fe645f28667de1fb1971
Corey <coreystock@gmail.com>
2019-11-11 16:21:10 -0800

    Cleaned up main.py, can handle more statements.

--- main.py
+++ main.py
@@ -9,18 +9,5 @@ def main():
     parser = RDP()
     parser.parse()
     parser.write_output()
-    # files = sys.argv[1:]
-    
-    # for file in files:
-    #     lex = Lexer(file=file)
-    #     tokens = []
 
-    #     while not lex.done:
-    #         token = lex.lexer()
-    #         if token and token.t_type != 'whitespace':
-    #             tokens.append(token)
-
-    #     with open("output_{}.txt".format(file), 'w') as output:
-    #         for t in tokens:
-    #             output.write('{}      {}\n'.format(t.t_type, t.t_value))
 main()
